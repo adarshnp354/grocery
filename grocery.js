3a7ae321c3b77a9414db5f08c61e93d50c74bde6
adarsh354 <fullstack_trainee_001@thefuturalabs.com>
2020-12-03 10:42:57 +0530

    Update grocery.js

--- grocery.js
+++ grocery.js
@@ -1,17 +1,26 @@
 function load(){
 var req,data,content,x,txt="";
-data="file:///C:/Users/acer/Desktop/Adarsh_Task/grocery/grocery.json";
 req = new XMLHttpRequest();
 req.onreadystatechange = function(){
-    if(req.readyState==4){
-        content=JSON.parse(req.responseText);
-        document.getElementById("slno").innerHTML=content.slno;
-        document.getElementById("name").innerHTML=content.name;
-        document.getElementById("quantity").innerHTML=content.qty;
-        document.getElementById("unit").innerHTML=content.unit;
-        document.getElementById("dept").innerHTML=content.dept;
+    if(this.readyState===4){
+        content=JSON.parse(this.responseText);
+        data=content.list;
+        for(i=0;i<data.length;i++){
+            txt+="<tr>"
+            txt+="<th>"+data[i].slno+"</th>"
+            txt+="<th>"+data[i].name+"</th>"
+            txt+="<th>"+data[i].qty+"</th>"
+            txt+="<th>"+data[i].unit+"</th>"
+            txt+="<th>"+data[i].dept+"</th>"
+            txt+="</tr>"
+        }
+        document.getElementById("myTable").innerHTML=data.slno;
+        document.getElementById("myTable").innerHTML=data.name;
+        document.getElementById("myTable").innerHTML=data.qty;
+        document.getElementById("myTable").innerHTML=data.unit;
+        document.getElementById("myTable").innerHTML=data.dept;
     }
 };
-req.open("GET",data,true);
+req.open("GET","grocery.json",true);
 req.send();
 }
